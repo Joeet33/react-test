19c362e00ad0ad529e5e6b7480c32793f2122a67
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-11-02 15:52:03 +0000

    fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [coins, setCoins] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    const data = await res.json()
+    setCoins(data)
+    }
+    fetchData()
+  },[])
+  
   return (
-    <div className="App">
+    <div>
     </div>
   );
 }
